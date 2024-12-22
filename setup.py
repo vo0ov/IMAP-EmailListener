eba4a6e839f55498d8061eca4083af17fdd2e8dd
vo0ov <testvo0ovtest@gmail.com>
2024-12-22 20:08:49 +0300

    Commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='EmailListener',
+    name='IMAP-EmailListener',
     version='1.0.0',
     author='vo0ov',
     author_email='artgr123@yandex.ru',
     description='Библиотека для прослушивания и обработки электронных писем через IMAP.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/vo0ov/EmailListener',
+    url='https://github.com/vo0ov/IMAP-EmailListener',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
